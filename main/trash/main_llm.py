2c4b5c49a36f6c60e7005834266ad6d75ef9da85
acantelli-csw <ACantelli@centrosoftware.com>
2025-10-27 10:39:41 +0100

    deleting old useless stuff

--- main/trash/main_llm.py
+++
@@ -1,8 +0,0 @@
-from llm import ask_gpt
-
-prompt = "Descrivi brevemente il machine learning"
-
-answer = ask_gpt(prompt)
-
-print("Risposta LLM:")
-print(answer)
--- main/trash/prova.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-# percorso relativo al file stesso dello script
-file_path = Path(__file__).parent / "prompts" / "prompt_1a.txt"
-
-# leggi tutto il testo
-full_text = file_path.read_text(encoding="utf-8")
-
-print(full_text)
